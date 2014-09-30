220f63c06f8bdb392c0827458e417ef86260beeb
tpluscode <tpluscode@users.noreply.github.com>
2014-09-30 12:47:52 +0200

    don't remvoe readm from gh-pages branch

--- scripts/prepareGhPages.sh
+++ scripts/prepareGhPages.sh
@@ -4,7 +4,7 @@ rm -rf dist
 git clone https://github.com/tpluscode/jsonld-hydra-presentation.git dist
 cd dist
 git checkout gh-pages
-rm -rf *
+find . -maxdepth 1 -not -name 'README.md' -not -path '*/\.*' | xargs rm -rf
 git config --global user.email "tpluscode@users.noreply.github.com"
 git config --global user.name "tpluscode"
 cd ..
